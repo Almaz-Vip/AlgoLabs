e96868b93f91cf55825657ba50ec5da8f26e74eb
zerod <awunefieglfjwn@gmail.com>
2026-09-12 08:55:10 +0600

    Исправлены алгоритмы и тесты

--- src/main/java/ru/almazvip/lab1/FirstSolution.java
+++ src/main/java/ru/almazvip/lab1/FirstSolution.java
@@ -1,4 +1,32 @@
 package ru.almazvip.lab1;
 
+import java.util.Stack;
+
 public class FirstSolution {
+    public static boolean isValidParentheses(String data) {
+        if (data == null) { throw new NullPointerException(); }
+
+        var chars = new Stack<Character>();
+
+        for (int i = 0; i < data.length(); i++) {
+            char current = data.charAt(i);
+
+            if (current == '(' || current == '[' || current == '{') {
+                chars.push(current);
+            } else {
+                if (chars.isEmpty()) {
+                    return false;
+                }
+
+                char top = chars.pop();
+                if ((current == ')' && top != '(') ||
+                        (current == ']' && top != '[') ||
+                        (current == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+
+        return chars.isEmpty();
+    }
 }
--- src/test/java/ru/almazvip/lab1/SecondSolutionTest.java
+++ src/test/java/ru/almazvip/lab1/SecondSolutionTest.java
@@ -1,4 +1,73 @@
 package ru.almazvip.lab1;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SecondSolutionTest {
+    @Test
+    public void secondSolution_mixedElements() {
+        int[] arr = {3, 2, 2, 3};
+        int val = 3;
+        int newLength = SecondSolution.removeElementInplace(arr, val);
+
+        assertEquals(2, newLength, "Длина должна быть 2");
+
+        assertEquals(2, arr[0]);
+        assertEquals(2, arr[1]);
+    }
+
+    @Test
+    public void secondSolution_allElementsAreVal() {
+        int[] arr = {2, 2, 2, 2};
+        int val = 2;
+        int newLength = SecondSolution.removeElementInplace(arr, val);
+
+        assertEquals(0, newLength, "Если все элементы удалены, длина должна быть 0");
+    }
+
+    @Test
+    public void secondSolution_noElementsAreVal() {
+        int[] arr = {1, 4, 5, 6};
+        int val = 99;
+        int newLength = SecondSolution.removeElementInplace(arr, val);
+
+        assertEquals(4, newLength, "Длина не должна измениться");
+
+        assertEquals(1, arr[0]);
+        assertEquals(4, arr[1]);
+        assertEquals(5, arr[2]);
+        assertEquals(6, arr[3]);
+    }
+
+    @Test
+    public void secondSolution_emptyArray() {
+        int[] arr = {};
+        int val = 1;
+        int newLength = SecondSolution.removeElementInplace(arr, val);
+
+        assertEquals(0, newLength, "Для пустого массива длина всегда 0");
+    }
+
+    @Test
+    public void secondSolution_orderUnchanged() {
+        int[] arr = {0, 1, 2, 2, 3, 0, 4, 2};
+        int val = 2;
+        int newLength = SecondSolution.removeElementInplace(arr, val);
+
+        assertEquals(5, newLength);
+
+        int[] expectedPrefix = {0, 1, 3, 0, 4};
+        for (int i = 0; i < newLength; i++) {
+            assertEquals(expectedPrefix[i], arr[i], "Элемент на индексе " + i + " не совпадает");
+        }
+    }
+
+    @Test
+    public void secondSolution_nullArrayInput() {
+        int val = 13;
+
+        assertThrows(NullPointerException.class, () -> {
+            SecondSolution.removeElementInplace(null, val);
+        }, "Передача null должна вызывать NullPointerException");
+    }
 }
