f40d431082968a6c2b2aac520fdebd2cc1a618ef
zerod <awunefieglfjwn@gmail.com>
2026-09-18 14:01:29 +0600

    main.main

--- src/main/java/ru/almazvip/Main.java
+++ src/main/java/ru/almazvip/Main.java
@@ -2,6 +2,6 @@ package ru.almazvip;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello ,wroDL!");
+        System.out.println("Hello, world!");
     }
 }
