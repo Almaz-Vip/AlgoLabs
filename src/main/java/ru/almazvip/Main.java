2b39d3c41ddaa1c54d225974f76a3af16585625a
zerod <awunefieglfjwn@gmail.com>
2026-09-19 14:45:00 +0600

    Очень важное изменение

--- src/main/java/ru/almazvip/Main.java
+++ src/main/java/ru/almazvip/Main.java
@@ -7,12 +7,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        var arr = new ArrayList<Integer>(List.of(5, 2, 6, 8, 3, 1, 9));
-        MergeSort sort = new MergeSort();
-        sort.sort(arr);
-        for (int i = 0; i < arr.size(); i++) {
-            IO.print(arr.get(i));
-            IO.print(" ");
-        }
+        IO.println("Питон > Java");
     }
 }
